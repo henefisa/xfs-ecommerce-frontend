95464958018a74d51b463d2234174a7f142e7ff8
Henefisa <cherishnghia@gmail.com>
2021-08-03 19:48:16 +0700

    responsive header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,9 +5,18 @@ import Container from "../Container/Container";
 
 const Header: React.FC = () => {
   const headerRef = useRef<HTMLDivElement>(null);
+  const toggleRef = useRef<HTMLDivElement>(null);
+
+  const handleToggleMenu = () => {
+    toggleRef.current?.classList.toggle("header__toggle--active");
+    headerRef.current?.classList.toggle("header--active");
+  };
 
   useEffect(() => {
     const scroll = () => {
+      if (!window.matchMedia("(min-width: 768px)").matches) {
+        return;
+      }
       if (window.scrollY > 0) {
         headerRef.current?.classList.add("header--scrolled");
       } else {
@@ -26,7 +35,18 @@ const Header: React.FC = () => {
       <div className="header__menu">
         <Container className="header__menu-content">
           <h1 className="header__logo">Ecommerce</h1>
-          <nav>
+          <div
+            className="header__toggle"
+            ref={toggleRef}
+            onClick={handleToggleMenu}
+          >
+            <div className="bars">
+              <span className="bars__line" />
+              <span className="bars__line" />
+              <span className="bars__line" />
+            </div>
+          </div>
+          <nav className="nav">
             <ul className="menu">
               <li className="menu__item">Sample</li>
               <li className="menu__item">Sample</li>
