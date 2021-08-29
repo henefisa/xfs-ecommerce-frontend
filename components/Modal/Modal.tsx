1b2ec7970e234cc35e97962b60b0e466f4a57015
Henefisa <cherishnghia@gmail.com>
2021-08-29 11:58:14 +0700

    add modaclass name

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -7,10 +7,16 @@ import ReactDOM from "react-dom";
 interface ModalProps {
   children: React.ReactNode;
   isOpen?: boolean;
+  className?: string;
   onClose?: () => void;
 }
 
-const Modal: React.FC<ModalProps> = ({ children, isOpen, onClose }) => {
+const Modal: React.FC<ModalProps> = ({
+  children,
+  isOpen,
+  className,
+  onClose,
+}) => {
   const [root, setRoot] = useState<Element>();
 
   useEffect(() => {
@@ -19,7 +25,7 @@ const Modal: React.FC<ModalProps> = ({ children, isOpen, onClose }) => {
 
   return root
     ? ReactDOM.createPortal(
-        <div className={clsx("modal", isOpen && "modal--open")}>
+        <div className={clsx("modal", isOpen && "modal--open", className)}>
           <div className="modal__content">{isOpen ? children : null}</div>
           <div className="modal__close" onClick={onClose}>
             <FontAwesomeIcon icon={faTimes} />
