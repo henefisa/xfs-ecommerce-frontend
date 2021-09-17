b8100d6c4d76f7ae28d0d3a8a77ae613be87733d
Henefisa <cherishnghia@gmail.com>
2021-09-17 10:02:41 +0700

    forward ref input component

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -5,17 +5,21 @@ export interface InputProps {
   size?: "small" | "default" | "large";
 }
 
-const Input: React.FC<
-  InputProps & Omit<React.InputHTMLAttributes<HTMLInputElement>, "size">
-> = ({ className, size = "default", ...props }) => {
+const Input = React.forwardRef<
+  HTMLInputElement,
+  InputProps & React.InputHTMLAttributes<HTMLInputElement>
+>(({ size = "default", className, ...props }, ref) => {
   return (
     <div className={clsx("input-wrap", className)}>
       <input
         {...props}
         className={clsx("input", size !== "default" && `input--${size}`)}
+        ref={ref}
       />
     </div>
   );
-};
+});
+
+Input.displayName = "Input";
 
 export default React.memo(Input);
